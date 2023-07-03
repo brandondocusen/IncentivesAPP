3ada981ad7e514490a1980d0500c870214c0d784
Brandon Docusen <brandondocusen@gmail.com>
2023-07-02 22:53:23 -0500

    This is the initial commit for the incentives app
    Maven, Spring Boot, Spring Initializr, H2 database (default for testing), HTML/Javascript front-end, etc. Work in progress!

---
+++ demo/src/main/java/spring_initializer_workbrain/WorkbrainPrototypeApplication.java
@@ -0,0 +1,13 @@
+package spring_initializer_workbrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkbrainPrototypeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkbrainPrototypeApplication.class, args);
+	}
+
+}
---
+++ demo/src/test/java/spring_initializer_workbrain/WorkbrainPrototypeApplicationTests.java
@@ -0,0 +1,13 @@
+package spring_initializer_workbrain;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkbrainPrototypeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
