43616fb1b018b6d6d92e3c81550dc840d92f0111
Brandon Docusen <brandondocusen@gmail.com>
2023-07-11 00:45:18 -0500

    Bug squashed and dependency version updates
    • A bug was causing API posts to fail. The problem was that I had declared five columns to the students database table but was only posting four
    • Updated Spring Boot  to version 2.7.13 
    • Cleaned some old, unnecessary lines from various classes
    
    The next step is to add functionality beyond the current get and post API endpoint capabilities. Put, patch and delete endpoints will be installed. Once the put, patch and delete HTTP methods are implemented I will begin work on the integration tests card to validate the system before moving on to wiring up the front end.

---
+++ Reworkedbrain/src/main/java/com/spring/crud/demo/SpringBootH2CRUDApplication.java
@@ -0,0 +1,29 @@
+package com.spring.crud.demo;
+
+import com.spring.crud.demo.model.Student;
+import com.spring.crud.demo.repository.StudentRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+
+
+
+@Slf4j
+@SpringBootApplication
+@EnableJpaRepositories
+public class SpringBootH2CRUDApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootH2CRUDApplication.class, args);
+	}
+
+	@Autowired
+	private StudentRepository studentRepository;
+	
+
+
+}
